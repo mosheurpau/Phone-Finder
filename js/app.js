e8869b31b357cf90acca70dd74f9c3fefe607a70
mosheurpau <mosheur.pau@gamil.com>
2022-03-02 01:27:34 +0600

    single phone detail

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ const displaySearchResults = phones => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 border-0">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">${phone.brand}</p>
@@ -39,5 +39,36 @@ const loadPhoneDetail = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data.mainFeatures));
+        .then(data => displayPhoneDetail(data.data));
+}
+
+// Phone Details
+const displayPhoneDetail = phone => {
+    // console.log(meal);
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h3 class="card-title">Phone Name: ${phone.name}</h3>
+            <p class="card-text"><spen class="fw-bold">Phone Brand:</spen> ${phone.brand}</p>
+            <p class="card-text"><spen class="fw-bold">Release Date:</spen> ${phone.releaseDate}</p>
+            <p class="card-text"><spen class="fw-bold">Storage:</spen> ${phone.mainFeatures.storage}</p>
+            <p class="card-text"><spen class="fw-bold">Display Size: </spen> ${phone.mainFeatures.displaySize}</p>
+            <p class="card-text"><spen class="fw-bold">ChipSet: </spen> ${phone.mainFeatures.chipSet}</p>
+            <p class="card-text"><spen class="fw-bold">Memory: </spen> ${phone.mainFeatures.memory}</p>
+            <p class="card-text"><spen class="fw-bold">Sensors: </spen> ${phone.mainFeatures.sensors}</p>
+            <p class="card-text"><spen class="fw-bold">Others: </spen> ${phone.others.WLAN}.</p>
+            <p class="card-text"><spen class="fw-bold">Bluetooth: </spen> ${phone.others.Bluetooth}</p>
+            <p class="card-text"><spen class="fw-bold">GPS: </spen> ${phone.others.GPS}</p>
+            <p class="card-text"><spen class="fw-bold">NFC: </spen> ${phone.others.NFC}</p>
+            <p class="card-text"><spen class="fw-bold">Radio: </spen> ${phone.others.Radio}</p>
+            <p class="card-text"><spen class="fw-bold">USB: </spen> ${phone.others.USB}</p>
+            <p class="card-text"><spen class="fw-bold">USB: </spen> ${phone.others.USB}</p>
+
+        </div>
+    `;
+    phoneDetails.appendChild(div);
 }
