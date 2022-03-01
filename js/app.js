9bfaf37db2bb35657cdd916a87dd500b9e373b1e
mosheurpau <mosheur.pau@gamil.com>
2022-03-01 23:30:28 +0600

    added phone detail api link and button

--- js/app.js
+++ js/app.js
@@ -13,22 +13,31 @@ const searchPhone = () => {
         .then(data => displaySearchResults(data.data));
 }
 const displaySearchResults = phones => {
-    console.log(phones);
+    // console.log(phones);
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     phones.forEach(phone => {
-        console.log(phone.image);
+        // console.log(phone.image);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 border-0">
-                <img src="${phone.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${phone.phone_name}</h5>
-                    <p class="card-text">${phone.brand}</p>
-                </div>
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+                <button onclick="loadPhoneDetail('${phone.slug}')" >Detail</button>
             </div>
+        </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadPhoneDetail = phoneId => {
+    // console.log(phoneId);
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data.mainFeatures));
 }
